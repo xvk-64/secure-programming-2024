da1a4a99146360dfba9b3b1980d9ec6ac41d8283
maple-syrup404 <winter.roses265@protonmail.com>
2024-09-24 17:39:38 +0930

    added form that somewhat works for new groups

--- pkg/client/src/CreateGroupChat.tsx
+++ pkg/client/src/CreateGroupChat.tsx
@@ -1,8 +1,4 @@
 import React, {useEffect, useState} from "react";
-
-
-export default function CreateGroupChat() {
-
    
     // msgList.forEach((user) => {
     //     chatbox.push(
@@ -10,24 +6,61 @@ export default function CreateGroupChat() {
     //     );
     // });
 
+    // const [groupChatName, setGroupChatName] = useState("");
+
+    // const handleSubmit = (event: { preventDefault: () => void; }) => {
+    //   event.preventDefault();
+    //   alert(`The name you entered was: ${groupChatName}`)
+    // }
+
+
+export default function CreateGroupChat() {
+
+    const [inputs, setInputs] = useState({});
+    // store group members as group
+    let groupList: React.ReactElement[] = []
+
+    // event handler to grab values from fields
+    const handleChange = (event: { target: { name: string; value: string; }; }) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+        // nickanme : publickey
+    // store the members
+    const handleSubmit = (event: { preventDefault: () => void; }) => {
+      event.preventDefault();
+    //   alert(inputs.nickname+inputs.publicKey);
+    //   groupList.push(inputs)
+    //   console.log(groupList)
+     
+    }
+    let groupCounter = 1;
+
     return (
         <>
-        <h4 className="heading">Chat Name</h4>
         <div className="createChatDiv">
             <h4 className="heading">Create Groupchat</h4>
-            <h4> Groupchat name</h4>
-            <label>Visible only to you!</label><br/>
-            <input type="text"/><br/>
+            <form onSubmit={handleSubmit}>
             <h4>Add users to Group</h4>
-            <label>Enter saved nickname, OR enter user's public key</label><br/><br/>
-            <label>Search contact list for nickname</label><br/>
-            <input type="text"/>
-            <p>   OR   </p>
+            {/* <label>Enter saved nickname, OR enter user's public key</label><br/><br/> */}
+
+            <label>add nickname</label><br/>
+            {/* get nickname*/}
+            <input type="text" name="nickname" value={inputs.nickname || ""} onChange={handleChange}/>
+       
             <label>User's public key</label><br/>
-            <input type="text"/><br/>
+            {/* get public key*/}
+            <input type="text" name="publicKey" value={inputs.publicKey || ""} onChange={handleChange}/><br/>
+
+            {/* add user to working group*/}
             <button type="submit">Add user to group</button><br/>
 
             <h4>Group Members</h4><br/>
+             {/* finalise group*/}
+             </form>
             <button type="submit"> Create group! </button>
 
 
