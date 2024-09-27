73ca2f6cbc2f0f4aa4617c0e4496b7a82abc91e7
none <none>
2024-09-27 11:05:07 +0930

    cherry pick usercontext

---
+++ pkg/client/src/UserContext.tsx
@@ -0,0 +1,116 @@
+import { createContext, useEffect, useState } from "react";
+
+export interface GroupInfo {
+    users: string[],
+}
+
+export interface Group {
+    groupInfo: GroupInfo,
+    chatLog: string[],
+}
+
+export interface User {
+    // the user's keys
+    keyPair: CryptoKeyPair,
+    // the user's friends map
+    friends: Map<string, string>,
+    // the user's groups (with chat logs)
+    groups: Group[],
+    // the user's known servers
+    servers: string[],
+}
+
+export type UserContext = {
+    userKeys: CryptoKeyPair | null,
+    setUser: (user: User) => void,
+    friends: Map<string, string>,
+    updateFriend: (publicKey: string, newUsername: string) => void,
+    removeFriend: (publicKey: string) => void,
+    groups: Group[],
+    addGroup: (group: Group) => void,
+    servers: string[],
+    addServer: (server: string) => void,
+}
+
+// TODO: map out this type
+export const UserContext: React.Context<UserContext | null> = createContext<UserContext | null>(null);
+
+// TODO: what is this type?
+export const UserProvider = ({ children }: any) => {
+    const [friends, setFriends] = useState(new Map<string, string>());
+    const updateFriend = (publicKey: string, newUsername: string) => {
+        setFriends(prevFriends => {
+            const newMap = new Map(prevFriends);
+            newMap.set(publicKey, newUsername);
+            return newMap;
+        });
+    } // check for name collision
+    const removeFriend = (publicKey: string) => {
+        setFriends(prevFriends => {
+            const newMap = new Map(prevFriends);
+            newMap.delete(publicKey);
+            return newMap;
+        });
+    }
+    
+    const [groups, setGroups] = useState<Group[]>([]);
+    const addGroup = (group: Group) => {
+        setGroups(prevGroups => [...prevGroups, group])
+    }
+    
+    const [servers, setServers] = useState<string[]>([]);
+    const addServer = (server: string) => {
+        setServers(prevServers => [...prevServers, server])
+    }
+    
+    const [userKeys, setUserKeys] = useState<CryptoKeyPair | null>(null);
+
+    const setUser = (user: User) => {
+        setUserKeys(user.keyPair);
+        setFriends(user.friends);
+        setGroups(user.groups);
+        setServers(user.servers);
+    }
+
+    // then initialise it by checking the localstorage
+    useEffect(() => {
+        let localKeyPair = localStorage.getItem("keyPair");
+        if(localKeyPair) {
+            setUserKeys(JSON.parse(localKeyPair)); // this is not how this is supposed to be loaded
+            let localFriends = localStorage.getItem("friends");
+            if(localFriends) {setFriends(new Map(Object.entries(JSON.parse(localFriends))));}
+            let localGroups = localStorage.getItem("groups");
+            if(localGroups) {setGroups(JSON.parse(localGroups));}
+            let localServers = localStorage.getItem("servers");
+            if(localServers) {setServers(JSON.parse(localServers));}
+        }
+    }, []);
+    useEffect(() => {
+        localStorage.setItem("friends", JSON.stringify(friends));
+    }, [friends]);
+    useEffect(() => {
+        localStorage.setItem("groups", JSON.stringify(groups));
+    }, [groups]);
+    useEffect(() => {
+        localStorage.setItem("servers", JSON.stringify(servers));
+    }, [servers]);
+    useEffect(() => {
+        // no idea if that's a valid way of storing the keys
+        localStorage.setItem("keyPair", JSON.stringify(userKeys));
+    }, [userKeys]);
+
+    const ret = {
+        userKeys,
+        setUser,
+        friends,
+        updateFriend,
+        removeFriend,
+        groups,
+        addGroup,
+        servers,
+        addServer,
+    };
+    return <UserContext.Provider value={ret}>
+        {children}
+    </UserContext.Provider>
+}
