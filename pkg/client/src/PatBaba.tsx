5a831e61325d8d163c71d0b8e764e7a5e6e2576e
maple-syrup404 <winter.roses265@protonmail.com>
2024-09-28 14:58:56 +0930

    cannot get gif working  :(

--- pkg/client/src/PatBaba.tsx
+++ pkg/client/src/PatBaba.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react";
-import baba from './baba.gif';
+// import baba from './baba.gif';
 
 export default function PatBaba() {
     return (
-        <div><img src={baba} alt="baba is pet"/></div>
+        <div>
+            <p>Baba would be here if I could get gifs working</p>
+            {/* <img src={baba} alt="baba is pet"/> */}
+            </div>
     )
 }
