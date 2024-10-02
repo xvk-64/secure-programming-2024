023b69035f020b16430dffeae64af44d50b8a770
Valen Kostich <18544814+SpiccyMayonnaise@users.noreply.github.com>
2024-10-03 01:23:22 +0930

    Set up newclient

---
+++ pkg/newclient/_esbuild.dev.js
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild';
+import fs from "fs";
+
+fs.copyFileSync("./src/index.html", "./dist/index.html");
+
+const ctx = await esbuild.context({
+    entryPoints: ['./src/index.tsx'],
+    outdir: './dist',
+    bundle: true,
+    logLevel: 'info'
+});
+
+await ctx.watch();
+
+const { host, port } = await ctx.serve({
+    servedir: './dist/',
+});
