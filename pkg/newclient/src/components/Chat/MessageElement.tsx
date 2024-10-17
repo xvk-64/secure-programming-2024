de06d4a7a8748b246a0e531a7b29382dcd86cb01
maple-syrup404 <winter.roses265@protonmail.com>
2024-10-17 21:37:12 +1030

    made the : bold too

--- pkg/newclient/src/components/Chat/MessageElement.tsx
+++ pkg/newclient/src/components/Chat/MessageElement.tsx
@@ -8,7 +8,7 @@ export type MessageElementProps = {
 export function MessageElement(props: MessageElementProps) {
     return (
         <div>
-            <strong>{props.message.senderFingerprint}</strong>:
+            <strong>{props.message.senderFingerprint}:</strong>
             <span> {props.message.text}</span>
         </div>
     )
